af9d4af4ba9c4510563127a6fcdaf41509610efd
PedroRodante <pedrorodantevicente@gmail.com>
2023-05-22 18:09:16 -0300

    Alteração no log

--- Back-Node/app.js
+++ Back-Node/app.js
@@ -19,13 +19,13 @@ app.get("/", function(req,res){
 
 app.get("/funcao1", function (req, res){
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("Alguém passou por aqui");
+    console.log("Alguém passou na função 1!");
     res.send("CONECTOU!! Só isso aqui já um 8 em!? Vamos seguir...");     
 });
 
 app.post("/funcao2", function (req, res){
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("Alguém passou por aqui");
+    console.log("Alguém passou na função 2");
     let n1 = parseInt(req.body.n1);
     let n2 = parseInt(req.body.n2);
     n3 = n1 + n2;
@@ -34,13 +34,13 @@ app.post("/funcao2", function (req, res){
 
 app.get("/funcao3", function(req,res){
     res.header("Access-Control-Allow-Origin","*");
-    console.log("Alguém passou por aqui");
+    console.log("Alguém passou na função 3a!");
     res.send(`Foi ${n3}!`);
 });
 
 app.post("/funcao3", function(req,res){
     res.header("Access-Control-Allow-Origin","*");
-    console.log("Alguém passou por aqui");
+    console.log("Alguém passou na função 3b!");
     n3 = 0;
     res.send(`Pronto! Agora o valor salvo no servidor é ${n3}.`);
 });
